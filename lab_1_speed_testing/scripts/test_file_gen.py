4cb8ede43ecd7b3adb7934af35197dedf84c50ab
fenix-125 <yurijpasichnyk11@gmail.com>
2020-02-13 01:06:49 +0200

    add lab 1 start work on the performance testing of double to string casting done: - basic project structure - basic CMake - First TestFunction class - PerformanceTester skeleton - data input generation script

---
+++ lab_1_speed_testing/scripts/test_file_gen.py
@@ -0,0 +1,27 @@
+# Script that generates text file with double values to test performance
+# of double-string casting function.
+
+from random import uniform
+
+
+def generate_text(file_name: str, f_size: int):
+    """
+    Function generates a text file (of size larger than 'size' in MB) of double values.
+    If the file already exist will raise an ERROR.
+
+    :param file_name: the name file to create
+    :param f_size: minimal size of the generated file in MB
+    """
+    with open(file_name, 'x', encoding="ASCII") as f:
+        file_size = 0
+        while file_size < f_size * 1000000:
+            str_num = str(uniform(-100, 100))
+            print(str_num, file=f)
+            file_size += len(str_num)
+
+
+if __name__ == '__main__':
+    try:
+        generate_text("../tmp/doubles_1MB.txt", 1)
+    except IOError as e:
+        print(f"Error file {str(e).split()[-1]} already exist! Use other file name or delete this file.")
